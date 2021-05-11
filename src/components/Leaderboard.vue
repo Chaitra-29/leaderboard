a56eed9dd60c03a6830a752e699268b15febad74
Chaitra bhagavan <chaitra.bhagavan@maersk.com>
2021-05-11 15:25:35 +0530

    remove api

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
-    <nav class="navbar sticky-top navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img :src="brandImage" class="avatar" alt="" />
+    <nav class='navbar sticky-top navbar-light bg-light'>
+      <a class='navbar-brand' href='#'>
+        <img :src='brandImage' class='avatar' alt='' />
         {{ brandName }}
       </a>
-      <span class="navbar-text">
-        <img :src="athleteImage" class="avatar" alt="" />
+      <span class='navbar-text'>
+        <img :src='athleteImage' class='avatar' alt='' />
       </span>
     </nav>
-    <table class="table table-striped">
+    <table class='table table-striped'>
       <thead>
         <tr>
-          <th scope="col">Rank</th>
-          <th scope="col">Athlete</th>
-          <th scope="col">Distance</th>
-          <th scope="col">Rides</th>
-          <th scope="col">Longest</th>
-          <th scope="col">Avg Speed</th>
-          <th scope="col">Elevation Gain</th>
+          <th scope='col'>Rank</th>
+          <th scope='col'>Athlete</th>
+          <th scope='col'>Distance</th>
+          <th scope='col'>Rides</th>
+          <th scope='col'>Longest</th>
+          <th scope='col'>Avg Speed</th>
+          <th scope='col'>Elevation Gain</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(activity, index) in activities" :key="index">
-          <th scope="row">{{ index + 1 }}</th>
+        <tr v-for='(activity, index) in activities' :key='index'>
+          <th scope='row'>{{ index + 1 }}</th>
           <td>{{ activity.athlete }}</td>
           <td>{{ activity.distance }} km</td>
           <td>{{ activity.rides }}</td>
@@ -44,27 +44,28 @@
 }
 </style>
 <script>
-import moment from "moment";
-import { utilities } from "./../common/utilities";
-import brandImage from "../assets/cyclingmonks.jpg";
-import userIcon from "../assets/userIcon.png";
+import moment from 'moment';
+import { utilities } from './../common/utilities';
+import brandImage from '../assets/cyclingmonks.jpg';
+import userIcon from '../assets/userIcon.png';
 export default {
   data() {
     return {
-      clubId: "675234",
+      clubId: '675234',
       headers: {},
       athleteImage: userIcon,
-      athleteName: "",
-      brandName: "CyclingMonks",
+      athleteName: '',
+      brandName: 'CyclingMonks',
       brandImage: brandImage,
       activities: [],
-      activityType: "Ride",
+      activityType: 'Ride',
+      domain: window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'https://hidden-island-62961.herokuapp.com/'
     };
   },
   beforeMount: function() {
     let accessToken = utilities.getAccessTokenFromCookie();
-    if (accessToken === "") {
-      this.$router.replace("/");
+    if (accessToken === '') {
+      this.$router.replace('/');
     } else {
       this.headers = { Authorization: `Bearer ${accessToken}` };
       this.getAthlete().then((response) => {
@@ -84,10 +85,10 @@ export default {
   methods: {
     async refreshAccessTokens() {
       const response = await fetch(
-        "http://localhost:3000/api/refreshAccessTokens",
+        `${this.domain}/api/refreshAccessTokens`,
         {
           headers: {
-            accept: "application/json",
+            accept: 'application/json',
           },
         }
       );
@@ -95,17 +96,17 @@ export default {
     },
 
     async getAthlete() {
-      const response = await fetch("https://www.strava.com/api/v3/athlete", {
+      const response = await fetch('https://www.strava.com/api/v3/athlete', {
         headers: this.headers,
       });
       return await response.json();
     },
     async saveAthlete(athlete) {
-      let response = await fetch("http://localhost:3000/api/athlete", {
-        method: "POST",
+      let response = await fetch(`${this.domain}/api/athlete`, {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json;charset=utf-8",
-          accept: "application/json",
+          'Content-Type': 'application/json;charset=utf-8',
+          accept: 'application/json',
         },
         body: JSON.stringify(athlete),
       });
@@ -113,7 +114,7 @@ export default {
       console.log(response);
     },
     async getActivities() {
-      const response = await fetch(`http://localhost:3000/api/activities`, {
+      const response = await fetch(`${this.domain}/api/activities`, {
         headers: this.headers,
       });
       return await response.json();
@@ -121,12 +122,12 @@ export default {
 
     async refreshAccessToken(accessToken) {
       let response = await fetch(
-        "http://localhost:3000/api/refreshAccessToken",
+        `${this.domain}/api/refreshAccessToken`,
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json;charset=utf-8",
-            accept: "application/json",
+            'Content-Type': 'application/json;charset=utf-8',
+            accept: 'application/json',
           },
           body: JSON.stringify({ accessToken }),
         }
@@ -135,7 +136,7 @@ export default {
     },
 
     setAthleteDetails(athlete) {
-      console.log("athlete--->", athlete);
+      console.log('athlete--->', athlete);
       this.athleteName = athlete.firstname;
       this.athleteImage = athlete.profile_medium;
     },
@@ -145,7 +146,7 @@ export default {
         const responseArray = response.data;
         responseArray.forEach((data) => {
           response = data.value;
-          console.log("athActivities--->", response);
+          console.log('athActivities--->', response);
           let athlete = {};
           let totalDistance = 0;
           let totalElevationGain = 0;
@@ -156,12 +157,12 @@ export default {
           );
           athlete.athlete = data.athleteName;
           const weekStartDay = moment()
-            .day("Monday")
+            .day('Monday')
             .toDate();
           response.forEach((activity) => {
             let isValidDate =
               moment(activity.start_date).isSameOrBefore(weekStartDay) &&
-              moment(activity.start_date).isAfter(moment("2021-4-01"));
+              moment(activity.start_date).isAfter(moment('2021-4-01'));
             if (activity.type === this.activityType && isValidDate) {
               rides++;
               let activityDistance = this.convertToTwoDecimal(
